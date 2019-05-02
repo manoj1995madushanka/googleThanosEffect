27c7f1fc37f51a341ab560cc94dd57138d2869c2
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-05-02 20:15:21 +0530

    set image script

---
+++ setImage.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+    // Example of a document-ready block
+    $(function(){
+          // Get your file name here
+          var imgfilename = "...";
+
+          // Set your file name
+          $('#imgLogo').attr('src', imgfilename);
+    });
+</script>
